e462ef07becf16ad9c1d498dea4ea8c0ba5ba124
AlexF <alexander.fairclough@newcastle.edu.au>
2020-09-09 12:11:16 +1000

    Add MPU6050 driver library

---
+++ mcha3500labs/TM_MPU6050/defines.h
@@ -0,0 +1,9 @@
+#ifndef TM_DEFINES_H
+#define TM_DEFINES_H
+
+/* Put your global defines for all libraries here used in your project */
+
+#define MPU6050_I2C             I2C1
+#define MPU6050_I2C_PINSPACK    TM_I2C_PinsPack_1
+
+#endif
