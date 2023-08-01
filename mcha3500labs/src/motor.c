23cafbccdcbe1d76019cd7fae70277db3bd02a36
leniehdg <114197050+leniehdg@users.noreply.github.com>
2023-08-01 15:11:40 +1000

    before starting on motor.c

---
+++ mcha3500labs/src/motor.c
@@ -0,0 +1,29 @@
+void motor_PWM_init(void)
+{
+/* TODO: Enable TIM3 clock */
+/* TODO: Enable GPIOA clock */
+
+/* TODO: Initialise PA6 with:
+ - Pin 6
+ - Alternate function push-pull mode
+ - No pull
+ - High frequency
+ - Alternate function 2 - Timer 3*/
+
+/* TODO: Initialise timer 3 with:
+ - Instance TIM3
+ - Prescaler of 1
+ - Counter mode up
+ - Timer period to generate a 10kHz signal
+ - Clock division of 0 */
+
+/* TODO: Configure timer 3, channel 1 with:
+ - Output compare mode PWM1
+ - Pulse = 0
+ - OC polarity high
+ - Fast mode disabled */
+
+/* TODO: Set initial Timer 3, channel 1 compare value */
+
+/* TODO: Start Timer 3, channel 1 */
+}
