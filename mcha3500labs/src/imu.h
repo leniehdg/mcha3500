50d24a7292af2a8ab066e5c1bcf9819ac0faa344
leniehdg <114197050+leniehdg@users.noreply.github.com>
2023-09-04 17:41:34 +1000

    added new imu files

---
+++ mcha3500labs/src/imu.h
@@ -0,0 +1,12 @@
+#ifndef IMU_H
+#define IMU_H
+
+void IMU_init(void);
+void IMU_read(void);
+float get_accY(void);
+// float get_accZ(void);
+// float get_gyroX(void);
+// float get_acc_angle(void);
+
+#endif
+
