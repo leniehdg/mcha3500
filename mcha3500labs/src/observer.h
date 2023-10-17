ea88dde676072ed0b64914fa37848be541db4ae6
leniehdg <114197050+leniehdg@users.noreply.github.com>
2023-10-17 13:45:40 +1100

    added observer h file

---
+++ mcha3500labs/src/observer.h
@@ -0,0 +1,10 @@
+#ifndef OBSERVER_H
+#define OBSERVER_H
+
+void observer_init(void);
+void observer_update(float y_measure, float y_measure2);
+float get_obs_x1(void);
+float get_obs_x2(void);
+
+
+#endif
