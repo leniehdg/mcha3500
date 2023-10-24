c440c9b688bb9d740916a8b0aa8de8d3d58c3cb6
leniehdg <114197050+leniehdg@users.noreply.github.com>
2023-10-24 19:17:18 +1100

    new controller from luke worksss

--- mcha3500labs/src/controller.h
+++ mcha3500labs/src/controller.h
@@ -1,26 +1,12 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-/* Add function prototypes here */
+float getControl(void);
+void ctrl_update(void);
 void ctrl_init(void);
 void ctrl_set_x1_int(float x1);
 void ctrl_set_x2_int(float x2);
-float getdPtheta(void);
-float getdtheta(void);
-float getControl(void);
-void ctrl_update(void);
-void ctrl_set_x_int(void);
 
 
 #endif
 
-enum {
-CTRL_N_INPUT = 1, // number of controller inputs (reference signals)
-CTRL_N_STATE = 4, // number of controller states (states)
-CTRL_N_OUTPUT = 1, // number of controller outputs / plant inputs
-CTRL_N_HORIZON = 10, // control horizon length
-CTRL_N_EQ_CONST = 0, // number of equality constraints
-CTRL_N_INEQ_CONST = 20, // number of inequality constraints
-CTRL_N_LB_CONST = 10, // number of lower bound constraints
-CTRL_N_UB_CONST = 10, // number of upper bound constraints
-};
